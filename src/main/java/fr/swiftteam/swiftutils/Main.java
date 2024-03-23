a68e7b9bfa66c0fc193062b0daa6b54328dab2eb
Your Name <snipertv59.pro@gmail.com>
2024-03-23 19:18:42 +0100

    🔨 Refactor some code.

--- src/main/java/fr/swiftteam/swiftutils/Main.java
+++ src/main/java/fr/swiftteam/swiftutils/Main.java
@@ -1,42 +1,51 @@
 package fr.swiftteam.swiftutils;
 
 import fr.swiftteam.swiftutils.managers.LoadingManager;
-import fr.swiftteam.swiftutils.modules.AnnouncementMessages.ModuleManagerAM;
+import fr.swiftteam.swiftutils.managers.ModulesManager;
 import fr.swiftteam.swiftutils.nms.NMSUtils;
 import fr.swiftteam.swiftutils.nms.NMS_v1_8_R3;
 import fr.swiftteam.swiftutils.utilities.ConsoleLogger;
-import fr.swiftteam.swiftutils.utilities.files.ConfigurationFile;
-import fr.swiftteam.swiftutils.utilities.files.MessagesFile;
 import fr.swiftteam.swiftutils.utilities.files.FilesManager;
-import fr.swiftteam.swiftutils.utilities.files.modules.AnnouncementMessagesFile;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitScheduler;
 
 public class Main extends JavaPlugin {
 
 	private static Main instance;
+	private static ConsoleLogger consoleLogger;
+
 	private static FilesManager filesManager;
+	private static ModulesManager modulesManager;
+	private static LoadingManager loadingManager;
+
 	private static BukkitScheduler bukkitScheduler;
 	private static NMSUtils nmsUtils;
 
-	private static final String configurationFileVersion = "1.1";
-	private static final String messagesFileVersion = "1.0";
+	private static final String latestConfigurationFileVersion = "1.0";
+	private static final String latestMessagesFileVersion = "1.0";
 
 
 	@Override
 	public void onEnable() {
 
 		instance = this;
+
+		consoleLogger = new ConsoleLogger();
+
 		filesManager = new FilesManager();
+		modulesManager = new ModulesManager();
+		loadingManager = new LoadingManager();
+
 		bukkitScheduler = this.getServer().getScheduler();
 
 		// PLUGIN NOW LOADING
-		ConsoleLogger.console("§7The plugin is now §6loading§7...");
+		consoleLogger.console("§7The plugin is now §6loading§7...");
+		consoleLogger.console("§7Version: §6" + getDescription().getVersion());
 
-		if (LoadingManager.loadPlugin()) {
+		if (loadingManager.loadPlugin()) {
 			// PLUGIN STARTED SUCCESSFULLY
-			ConsoleLogger.console("");
-			ConsoleLogger.console("§7The plugin has been §aloaded §7successfully!");
+			consoleLogger.console("");
+			consoleLogger.console("§7The plugin has been §aloaded §7successfully!");
 
 		} else {
 			// PLUGIN NOT STARTED, AND WITH A PROBLEM
@@ -48,18 +57,18 @@ public class Main extends JavaPlugin {
 	@Override
 	public void onDisable() {
 
-		ConsoleLogger.console("§7The plugin is now §6disabling§7...");
+		consoleLogger.console("§7The plugin is now §6disabling§7...");
 
-		ModuleManagerAM.stopLoadedAnnouncements();
+		modulesManager.disableModules();
 
-		ConsoleLogger.console("");
-		ConsoleLogger.console("§7The plugin has been §adisabled §7successfully!");
+		consoleLogger.console("");
+		consoleLogger.console("§7The plugin has been §adisabled §7successfully!");
 	}
 
 
 	public static boolean initializeNMSUtils() {
 
-		String serverVersion = LoadingManager.getServerVersion();
+		String serverVersion = loadingManager.getServerVersion();
 		if (serverVersion == null) {
 			return false;
 		}
@@ -76,18 +85,8 @@ public class Main extends JavaPlugin {
 	}
 
 
-	public static ConfigurationFile getConfigurationFile() {
-		return filesManager.getConfigurationFile();
-	}
-
-
-	public static MessagesFile getMessagesFile() {
-		return filesManager.getMessagesFile();
-	}
-
-
-	public static AnnouncementMessagesFile getAnnouncementMessagesFile() {
-		return filesManager.getAnnouncementMessagesFile();
+	public static ConsoleLogger getConsoleLogger() {
+		return consoleLogger;
 	}
 
 
@@ -96,13 +95,23 @@ public class Main extends JavaPlugin {
 	}
 
 
+	public static ModulesManager getModulesManager() {
+		return modulesManager;
+	}
+
+
+	public static LoadingManager getLoadingManager() {
+		return loadingManager;
+	}
+
+
 	public static String getConfigurationFileVersion() {
-		return configurationFileVersion;
+		return latestConfigurationFileVersion;
 	}
 
 
 	public static String getMessagesFileVersion() {
-		return messagesFileVersion;
+		return latestMessagesFileVersion;
 	}
 
 
