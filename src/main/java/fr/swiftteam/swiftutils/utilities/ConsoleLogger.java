b9305e8f41fd29194b5287cac6bc7744e4653552
Your Name <snipertv59.pro@gmail.com>
2024-03-23 19:13:08 +0100

    🔥 Removing "static" type for ConsoleLogger class's methods.

--- src/main/java/fr/swiftteam/swiftutils/utilities/ConsoleLogger.java
+++ src/main/java/fr/swiftteam/swiftutils/utilities/ConsoleLogger.java
@@ -5,22 +5,22 @@ import org.bukkit.Bukkit;
 public class ConsoleLogger {
 
 
-	public static void console(String infoMessage) {
+	public void console(String infoMessage) {
 		Bukkit.getConsoleSender().sendMessage("§8[§b§lSwift§6§lUtils§8] §7" + infoMessage);
 	}
 
 
-	public static void info(String infoMessage) {
+	public void info(String infoMessage) {
 		Bukkit.getLogger().info("[SwiftUtils] " + infoMessage);
 	}
 
 
-	public static void warning(String infoMessage) {
+	public void warning(String infoMessage) {
 		Bukkit.getLogger().warning("[SwiftUtils] " + infoMessage);
 	}
 
 
-	public static void error(String infoMessage) {
+	public void error(String infoMessage) {
 		Bukkit.getLogger().severe("[SwiftUtils] " + infoMessage);
 	}
 }
