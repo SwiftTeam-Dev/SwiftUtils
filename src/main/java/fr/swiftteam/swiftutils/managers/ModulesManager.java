269863a5d652bc9fdcfec00e3cb548337f82cd86
Your Name <snipertv59.pro@gmail.com>
2024-03-23 19:17:02 +0100

    ⚡ New methods, variable and refactor some code.

--- src/main/java/fr/swiftteam/swiftutils/managers/ModulesManager.java
+++ src/main/java/fr/swiftteam/swiftutils/managers/ModulesManager.java
@@ -2,28 +2,66 @@ package fr.swiftteam.swiftutils.managers;
 
 import fr.swiftteam.swiftutils.Main;
 import fr.swiftteam.swiftutils.modules.AnnouncementMessages.ModuleManagerAM;
-import fr.swiftteam.swiftutils.utilities.ConsoleLogger;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class ModulesManager {
 
+	private final ModuleManagerAM moduleManagerAM = new ModuleManagerAM();
 
-	public static void enableModules() {
+	private Map<String, Boolean> enabledModules = new HashMap<>();
 
-		ConsoleLogger.console("");
-		ConsoleLogger.console("§7The modules are now §6enabling§7...");
 
-		if (Main.getConfigurationFile().isModuleEnabled("announcementMessages")) {
-			if (ModuleManagerAM.loadAnnouncementMessagesModule()) {
-				ConsoleLogger.console("§8- §fAnnouncementMessages§7: §aLoaded successfully!");
+	public static List<String> getModules() {
+		return List.of(
+				"announcementMessages");
+	}
+
+
+	public void enableModules() {
+
+		Main.getConsoleLogger().console("");
+		Main.getConsoleLogger().console("§7The modules are now §6enabling§7...");
+
+		if (Main.getFilesManager().getConfigurationFile().isModuleEnabled("announcementMessages")) {
+			if (getAM().loadAnnouncementMessagesModule()) {
+				Main.getConsoleLogger().console("§8- §fAnnouncementMessages§7: §aLoaded successfully!");
 
 			} else {
-				ConsoleLogger.console("§8- §fAnnouncementMessages§7: §cError during loading!");
+				Main.getConsoleLogger().console("§8- §fAnnouncementMessages§7: §cError during loading!");
 			}
 
 		} else {
-			ConsoleLogger.console("§8- §fAnnouncementMessages§7: §6Disabled in §econfiguration.yml§6.");
+			Main.getConsoleLogger().console("§8- §fAnnouncementMessages§7: §6Disabled in §econfiguration.yml§6.");
 		}
+		Main.getConsoleLogger().console("§7The modules are now §aenabled§7!");
+	}
 
-		ConsoleLogger.console("§7The modules are now §aenabled§7!");
+
+	public void disableModules() {
+
+		getAM().unloadAnnouncementMessagesModule();
+	}
+
+
+	public void setModuleEnable(String moduleName) {
+		enabledModules.put(moduleName, true);
+	}
+
+
+	public void setModuleDisable(String moduleName) {
+		enabledModules.put(moduleName, false);
+	}
+
+
+	public boolean isEnabled(String moduleName) {
+		return enabledModules.get(moduleName);
+	}
+
+
+	public ModuleManagerAM getAM() {
+		return moduleManagerAM;
 	}
 }
