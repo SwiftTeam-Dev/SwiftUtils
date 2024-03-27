148d6b4a3cf97d8921267255f1a0f64a72340985
Sniper_TVmc <snipertv59.pro@gmail.com>
2024-03-27 21:16:42 +0100

    ⚡ Added player spawn module to module manager class

--- src/main/java/fr/swiftteam/swiftutils/managers/ModulesManager.java
+++ src/main/java/fr/swiftteam/swiftutils/managers/ModulesManager.java
@@ -1,7 +1,8 @@
 package fr.swiftteam.swiftutils.managers;
 
 import fr.swiftteam.swiftutils.Main;
-import fr.swiftteam.swiftutils.modules.AnnouncementMessages.ModuleManagerAM;
+import fr.swiftteam.swiftutils.modules.announcementMessages.ModuleManagerAM;
+import fr.swiftteam.swiftutils.modules.playerSpawn.ModuleManagerPS;
 
 import java.util.HashMap;
 import java.util.List;
@@ -10,13 +11,15 @@ import java.util.Map;
 public class ModulesManager {
 
 	private final ModuleManagerAM moduleManagerAM = new ModuleManagerAM();
+	private final ModuleManagerPS moduleManagerPS = new ModuleManagerPS();
 
 	private Map<String, Boolean> enabledModules = new HashMap<>();
 
 
 	public static List<String> getModules() {
 		return List.of(
-				"announcementMessages");
+				"announcementMessages",
+				"playerSpawn");
 	}
 
 
@@ -24,6 +27,8 @@ public class ModulesManager {
 
 		Main.getConsoleLogger().console("");
 		Main.getConsoleLogger().console("§7The modules are now §6enabling§7...");
+
+		// LOAD ANNOUNCEMENT MESSAGES MODULE
 
 		if (Main.getFilesManager().getConfigurationFile().isModuleEnabled("announcementMessages")) {
 			if (getAM().loadAnnouncementMessagesModule()) {
@@ -36,6 +41,23 @@ public class ModulesManager {
 		} else {
 			Main.getConsoleLogger().console("§8- §fAnnouncementMessages§7: §6Disabled in §econfiguration.yml§6.");
 		}
+
+		// LOAD PLAYER SPAWN MODULE
+
+		if (Main.getFilesManager().getConfigurationFile().isModuleEnabled("playerSpawn")) {
+			if (getPS().loadPlayerSpawnModule()) {
+				Main.getConsoleLogger().console("§8- §fPlayerSpawn§7: §aLoaded successfully!");
+
+			} else {
+				Main.getConsoleLogger().console("§8- §fPlayerSpawn§7: §cError during loading!");
+			}
+
+		} else {
+			Main.getConsoleLogger().console("§8- §fPlayerSpawn§7: §6Disabled in §econfiguration.yml§6.");
+		}
+
+		// END LOADING
+
 		Main.getConsoleLogger().console("§7The modules are now §aenabled§7!");
 	}
 
@@ -43,6 +65,7 @@ public class ModulesManager {
 	public void disableModules() {
 
 		getAM().unloadAnnouncementMessagesModule();
+		getPS().unloadPlayerSpawnModule();
 	}
 
 
@@ -63,5 +86,9 @@ public class ModulesManager {
 
 	public ModuleManagerAM getAM() {
 		return moduleManagerAM;
+	}
+
+	public ModuleManagerPS getPS() {
+		return moduleManagerPS;
 	}
 }
