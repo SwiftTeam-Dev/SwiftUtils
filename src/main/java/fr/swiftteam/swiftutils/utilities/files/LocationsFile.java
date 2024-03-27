bf90a360db3d726f5072ca16569d5fffaab8bab7
Sniper_TVmc <snipertv59.pro@gmail.com>
2024-03-27 14:36:53 +0100

    ⚡ Added new locations data file

---
+++ src/main/java/fr/swiftteam/swiftutils/utilities/files/LocationsFile.java
@@ -0,0 +1,22 @@
+package fr.swiftteam.swiftutils.utilities.files;
+
+import org.bukkit.configuration.file.YamlConfiguration;
+
+public class LocationsFile {
+
+	private final YamlConfiguration yamlConfiguration;
+
+
+	public LocationsFile(YamlConfiguration yamlConfiguration) {
+		this.yamlConfiguration = yamlConfiguration;
+	}
+
+
+	public String getSpawnLocation() {
+		String version = yamlConfiguration.getString("spawnLocation");
+		if (version != null) {
+			return version;
+		}
+		return null;
+	}
+}
